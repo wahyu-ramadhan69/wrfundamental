e7a22dc168aef9a22a8d9999266595f44f2c0559
wahyu-ramadhani69 <wahyu.ramadhani6969@gmail.com>
2023-08-15 13:14:27 +0700

    "commit ke ga tau"

--- src/main/java/com/bcafinance/Main.java
+++ src/main/java/com/bcafinance/Main.java
@@ -1,16 +1,11 @@
 package com.bcafinance;
 
+import java.util.Scanner;
+
 public class Main {
         public static void main(String[] args) {
-                Abc abc = new Abc();
-                System.out.println(abc);
-
-                int a = 10;
-                int b = 20;
-
-                int c = a * b;
-
-                System.out.println(c);
-
+                int a = 5;
+                a++;
+                System.out.println(a);
         }
 }
