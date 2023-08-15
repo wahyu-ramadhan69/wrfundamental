71fb727f66de001d35abddcf5b0e19c9d7218145
wahyu-ramadhani69 <wahyu.ramadhani6969@gmail.com>
2023-08-15 10:46:36 +0700

    "commit ke empat"

--- src/main/java/com/bcafinance/Main.java
+++ src/main/java/com/bcafinance/Main.java
@@ -8,7 +8,7 @@ public class Main {
                 int a = 10;
                 int b = 20;
 
-                int c = a + b;
+                int c = a * b;
 
                 System.out.println(c);
 
