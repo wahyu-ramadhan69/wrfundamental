6752e9dabd082855bb882ed0d9612492d6bc9a74
wahyu-ramadhani69 <wahyu.ramadhani6969@gmail.com>
2023-08-15 10:43:21 +0700

    commitan ke tiga

---
+++ src/main/java/com/bcafinance/Abc.java
@@ -0,0 +1,5 @@
+package com.bcafinance;
+
+public class Abc {
+
+}
--- src/main/java/com/bcafinance/Main.java
+++ src/main/java/com/bcafinance/Main.java
@@ -1,19 +1,16 @@
 package com.bcafinance;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
+                Abc abc = new Abc();
+                System.out.println(abc);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                int a = 10;
+                int b = 20;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+                int c = a + b;
+
+                System.out.println(c);
+
         }
-    }
 }
