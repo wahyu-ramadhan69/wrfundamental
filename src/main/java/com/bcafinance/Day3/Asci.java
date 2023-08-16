56099ae5984414b0a01840924024867ff26a3601
wahyu-ramadhani69 <wahyu.ramadhani6969@gmail.com>
2023-08-16 13:06:00 +0700

    push day 3

--- src/main/java/com/bcafinance/Day3/Asci.java
+++ src/main/java/com/bcafinance/Day3/Asci.java
@@ -8,19 +8,19 @@ public class Asci {
 
         System.out.println("******Progam penerjemah Ascii*******\n");
         Scanner sc = new Scanner(System.in);
-        String a = "a";
+        String a = "a123a";
+        System.out.println(a.toCharArray());
         int JumlahAsci = 0;
         int b;
         System.out.print("Masukkan data yang akan diterjemahkan : ");
-        a = sc.nextLine();
-        char[] chArr = a.toCharArray();
-        System.out.println(chArr);
-        for (int i = 0; i < chArr.length; i++) {
-            b = chArr[i];
-            if ((b >= 65 && b <= 90) || (b >= 97 && b <= 122)) {
-                JumlahAsci += b;
-            }
-        }
-        System.out.println(JumlahAsci);
+        // a = sc.nextLine();
+        // char[] chArr = a.toCharArray();
+        // for (int i = 0; i < chArr.length; i++) {
+        // b = chArr[i];
+        // if ((b >= 65 && b <= 90) || (b >= 97 && b <= 122)) {
+        // JumlahAsci += b;
+        // }
+        // }
+        // System.out.println(JumlahAsci);
     }
 }
